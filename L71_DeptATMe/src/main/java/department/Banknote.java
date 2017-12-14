db7274e25bf3c7dcc02fdc6de37d6bac421b01d8
stas-erogov <erogov.syu@gmail.com>
2017-12-14 21:47:00 +0500

    Сhanged Banknote more elegant solution

--- L71_DeptATMe/src/main/java/department/Banknote.java
+++ L71_DeptATMe/src/main/java/department/Banknote.java
@@ -1,40 +1,29 @@
 package department;
 
+import java.util.Arrays;
+
 public enum Banknote {
-    ONE,
-    FIVE,
-    TEN,
-    FIFTY,
-    ONE_HUNDRED;
+    ONE(1),
+    FIVE(5),
+    TEN(10),
+    FIFTY(50),
+    ONE_HUNDRED(100);
+
+    private int _nominal;
+
+    Banknote(int nominal) {
+        _nominal = nominal;
+    }
 
     public int getNominal() {
-        switch (this) {
-            case ONE:
-                return 1;
-            case FIVE:
-                return 5;
-            case TEN:
-                return 10;
-            case FIFTY:
-                return 50;
-            case ONE_HUNDRED:
-                return 100;
-        }
-        return 0;
+        return _nominal;
     }
 
     public static Banknote getBanknote(int value) {
-        switch (value) {
-            case 1:
-                return Banknote.ONE;
-            case 5:
-                return Banknote.FIVE;
-            case 10:
-                return Banknote.TEN;
-            case 50:
-                return Banknote.FIFTY;
-            case 100:
-                return Banknote.ONE_HUNDRED;
+        for (Banknote banknote : Banknote.values()) {
+            if (banknote.getNominal() == value){
+                return banknote;
+            }
         }
         return null;
     }
